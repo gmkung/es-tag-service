188cda2dd0ed763a42fa613f09c339c16eafb85a
gmkung <guangmian@gmail.com>
2024-06-19 15:37:51 +0800

    added user agent

--- dist/index.js
+++ dist/index.js
@@ -45,12 +45,13 @@ const allowedOrigins = ["https://script.google.com"];
 const corsOptions = {
     origin: function (origin, callback) {
         // Check if the origin is in the allowed list
-        if (allowedOrigins.indexOf(origin) !== -1) { //|| !origin 
-            console.log('Origin ok');
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            //
+            console.log("Origin ok");
             callback(null, true);
         }
         else {
-            callback(new Error("Not allowed by CORS"));
+            callback(new Error("Not allowed by CORS: " + origin));
         }
     },
 };
@@ -65,7 +66,15 @@ app.get("/returnExplorerTag", (req, res) => __awaiter(void 0, void 0, void 0, fu
     }
     try {
         // Fetch the web page
-        const { data } = yield axios_1.default.get(url);
+        const { data } = yield axios_1.default.get(url, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.9",
+                Connection: "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+            },
+        });
         // Load the HTML into cheerio
         const $ = cheerio.load(data);
         // Find and extract the contents of the specified element
--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,17 @@ app.get("/returnExplorerTag", async (req: Request, res: Response) => {
 
   try {
     // Fetch the web page
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+      },
+    });
 
     // Load the HTML into cheerio
     const $ = cheerio.load(data);
