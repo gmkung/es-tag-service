cdc78031dc68120f435994f874e154e7b11dcb84
gmkung <guangmian@gmail.com>
2024-06-19 11:11:19 +0800

    Added an endpoint to return explorer tag

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,9 +38,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cheerio = __importStar(require("cheerio"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config(); // Load environment variables from .env
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.post("/upsertAddress", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { contractAddress } = req.body;
@@ -77,6 +103,89 @@ app.post("/upsertAddress", (req, res) => __awaiter(void 0, void 0, void 0, funct
             // Fallback for unknown error types
             res.status(500).json({ error: "Unknown Error" });
         }
+    }
+}));
+app.post("/checkESLabelPresence", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { contractAddress } = req.body;
+    if (!contractAddress) {
+        return res.status(400).send("Bad Request: contractAddress is required");
+    }
+    const credId = "415873790728261632"; // Example credential ID
+    const operation = "APPEND"; // Example operation
+    const items = [contractAddress]; // Use the received contractAddress
+    try {
+        const result = yield axios_1.default.post("https://graphigo.prd.galaxy.eco/query", {
+            operationName: "credentialItems",
+            query: `
+          mutation credentialItems($credId: ID!, $operation: Operation!, $items: [String!]!) 
+          { 
+            credentialItems(input: { 
+              credId: $credId, 
+              operation: $operation, 
+              items: $items 
+            }) 
+            { 
+              name 
+            } 
+          }
+        `,
+            variables: {
+                credId: credId,
+                operation: operation,
+                items: items,
+            },
+        }, {
+            headers: {
+                "access-token": process.env.GALXE_ACCESS_TOKEN_c2F6, // Use the environment variable
+            },
+        });
+        if (result.status !== 200) {
+            throw new Error(`HTTP error: ${result.status}`);
+        }
+        else if (result.data.errors && result.data.errors.length > 0) {
+            console.log(result.data.errors);
+            throw new Error("GraphQL error: " + JSON.stringify(result.data.errors));
+        }
+        res.status(200).json(result.data);
+    }
+    catch (error) {
+        if (axios_1.default.isAxiosError(error)) {
+            // Axios-specific error handling
+            res.status(500).json({ error: "Axios Error", details: error.message });
+        }
+        else if (error instanceof Error) {
+            // General error handling
+            res
+                .status(500)
+                .json({ error: "Internal Server Error", details: error.message });
+        }
+        else {
+            // Fallback for unknown error types
+            res.status(500).json({ error: "Unknown Error" });
+        }
+    }
+}));
+//To be triggered and used exclusively by Etherscan's team
+app.get("/returnExplorerTag", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const url = req.query.url;
+    const elementSelector = ".hash-tag.text-truncate.lh-sm.my-n1";
+    if (!url || !elementSelector) {
+        return res.status(400).json({ error: "Missing url or selector parameter" });
+    }
+    console.log(url);
+    try {
+        // Fetch the web page
+        const { data } = yield axios_1.default.get(url);
+        // Load the HTML into cheerio
+        const $ = cheerio.load(data);
+        // Find and extract the contents of the specified element
+        const elementContent = $(elementSelector).text().trim();
+        // Return the extracted content
+        res.json({ content: elementContent });
+    }
+    catch (error) {
+        console.error("Error fetching the web page:", error);
+        res.status(500).json({ error: "Error fetching the web page" });
     }
 }));
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
-
+import * as cheerio from "cheerio";
 import cors from "cors";
 
 dotenv.config(); // Load environment variables from .env
@@ -76,6 +76,98 @@ app.post("/upsertAddress", async (req: Request, res: Response) => {
       // Fallback for unknown error types
       res.status(500).json({ error: "Unknown Error" });
     }
+  }
+});
+
+app.post("/checkESLabelPresence", async (req: Request, res: Response) => {
+  const { contractAddress } = req.body;
+
+  if (!contractAddress) {
+    return res.status(400).send("Bad Request: contractAddress is required");
+  }
+
+  const credId = "415873790728261632"; // Example credential ID
+  const operation = "APPEND"; // Example operation
+  const items = [contractAddress]; // Use the received contractAddress
+
+  try {
+    const result = await axios.post(
+      "https://graphigo.prd.galaxy.eco/query",
+      {
+        operationName: "credentialItems",
+        query: `
+          mutation credentialItems($credId: ID!, $operation: Operation!, $items: [String!]!) 
+          { 
+            credentialItems(input: { 
+              credId: $credId, 
+              operation: $operation, 
+              items: $items 
+            }) 
+            { 
+              name 
+            } 
+          }
+        `,
+        variables: {
+          credId: credId,
+          operation: operation,
+          items: items,
+        },
+      },
+      {
+        headers: {
+          "access-token": process.env.GALXE_ACCESS_TOKEN_c2F6, // Use the environment variable
+        },
+      }
+    );
+
+    if (result.status !== 200) {
+      throw new Error(`HTTP error: ${result.status}`);
+    } else if (result.data.errors && result.data.errors.length > 0) {
+      console.log(result.data.errors);
+      throw new Error("GraphQL error: " + JSON.stringify(result.data.errors));
+    }
+
+    res.status(200).json(result.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // Axios-specific error handling
+      res.status(500).json({ error: "Axios Error", details: error.message });
+    } else if (error instanceof Error) {
+      // General error handling
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", details: error.message });
+    } else {
+      // Fallback for unknown error types
+      res.status(500).json({ error: "Unknown Error" });
+    }
+  }
+});
+
+//To be triggered and used exclusively by Etherscan's team
+app.get("/returnExplorerTag", async (req: Request, res: Response) => {
+  const url: string = req.query.url as string;
+  const elementSelector = ".hash-tag.text-truncate.lh-sm.my-n1";
+  if (!url || !elementSelector) {
+    return res.status(400).json({ error: "Missing url or selector parameter" });
+  }
+
+  try {
+    // Fetch the web page
+    const { data } = await axios.get(url);
+
+    // Load the HTML into cheerio
+    const $ = cheerio.load(data);
+
+    // Find and extract the contents of the specified element
+    const elementContent = $(elementSelector).text().trim();
+
+    // Return the extracted content
+    res.json({ tag: elementContent });
+  } catch (error) {
+    console.error("Error fetching the web page:", error);
+    res.status(500).json({ error: "Error fetching the web page" });
   }
 });
 
